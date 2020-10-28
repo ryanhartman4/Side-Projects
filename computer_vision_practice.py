645fe882b3ca2cd859b61d6498d0dfe3cae86f44
Ryan Hartman <73214283+ryanhartman4@users.noreply.github.com>
2020-10-28 16:11:17 -0700

    Update computer_vision_practice.py

--- computer_vision_practice.py
+++ computer_vision_practice.py
@@ -7,6 +7,8 @@ Original file is located at
     https://colab.research.google.com/drive/1Ae2iR7UmMiYc16ZGd4lQQuBFi_QJoxR-
 """
 
+"""This basic computer vision project was based on an article I read earlier this year. I wanted to see if I could create something with a similar predicitve ability to the model article's author had created. I used the provided datasets and then fit a 2 layer sequential model to it. Afterwards, I took a look at the metrics of my model and tested its predictive ability. At the end I also provide anyone reading this an opportunity to see the model in action. In the future I hope to optimize this project further and find new ways to apply the knowledge I have gained from this project."""
+
 !wget --no-check-certificate \
     https://github.com/btphan95/simple-cnn/blob/master/train.zip?raw=true \
     -O /tmp/train.zip
